6eb4cae0081c36e36b056f59cf0853a80686b74c
dungnt <nguyentiendung11082004@gmail.com>
2026-01-07 15:27:23 +0700

    chore: remove be build artifacts from repository

--- be/build/routes/auth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const AuthRouter = (0, express_1.Router)();
-AuthRouter.post('/register', auth_1.register);
-exports.default = AuthRouter;
--- be/build/routes/students.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const student_1 = require("../controllers/student");
-const StudentRouter = (0, express_1.Router)();
-StudentRouter.post('/', student_1.getAllStudents);
-exports.default = StudentRouter;
